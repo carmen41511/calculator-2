05982a45ca235aa5794c4a19a303587b87c1b253
Carmen Liang <carmen41511@yahoo.com>
2022-11-09 16:27:22 -0500

    completed calculator function

--- calculator.py
+++ calculator.py
@@ -5,3 +5,41 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 
 
 # Replace this with your code
+while True:
+    user_input = input('Enter your equation >')
+    tokens = user_input.split()
+
+    if tokens[0] == '+':
+        ans = add(float(tokens[1]), float(tokens[2]))
+        print(ans)
+
+    elif tokens[0] == '-':
+        ans = subtract(float(tokens[1]), float(tokens[2]))
+        print(ans)
+
+    elif tokens[0] == '*':
+        ans = multiply(float(tokens[1]), float(tokens[2]))
+        print(ans)
+    
+    elif tokens[0] == '/':
+        ans = divide(float(tokens[1]), float(tokens[2]))
+        print(ans)
+
+    elif tokens[0] == 'square':
+        ans = square(float(tokens[1]))
+        print(ans)
+
+    elif tokens[0] == 'cube':
+        ans = cube(float(tokens[1]))
+        print(ans)
+    
+    elif tokens[0] == 'pow':
+        ans = power(float(tokens[1]), float(tokens[2]))
+        print(ans)
+
+    elif tokens[0] == 'mod':
+        ans = mod(float(tokens[1]), float(tokens[2]))
+        print(ans)
+
+    else:
+        break
